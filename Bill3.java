6c2c476160d080031ec28a9772c05da91aa89e99
ziveedri <ziv.edri01@runi.ac.il>
2024-11-07 18:58:37 +0200

    fixed again.

--- Bill3.java
+++ Bill3.java
@@ -4,8 +4,8 @@ public class Bill3 {
 	    String name1 = args[0];
 		String name2 = args[1];
 		String name3 = args[2];
-		int value = Integer.parseInt(args[3]);
-		System.out.println("dear "+name3+", "+name2+", and "+name1+": pay "+ Math.ceil((double)value/3)+" Shekels each.");
+		double value = Integer.parseInt(args[3]);
+		System.out.println("Dear " + name3 + ", " + name2 + ", and " + name1 + ": pay " + Math.ceil(value/3) + " Shekels each.");
 
 	}
 }
--- FVCalc.java
+++ FVCalc.java
@@ -4,6 +4,6 @@ public class FVCalc {
 		int currentValue = Integer.parseInt(args[0]);
 		double rate = Double.parseDouble(args[1]);
 		int n = Integer.parseInt(args[2]);
-		System.out.println("After "+n+" years, $"+currentValue+" saved at "+rate+"% will yield $"+(int)(currentValue*Math.pow((1+(rate/100)),n)));
+		System.out.println("After " + n + " years, a $" + currentValue + " saved at " + rate + "% will yield $" + (int) (currentValue*Math.pow((1+(rate/100)),n)));
 	}
 }
