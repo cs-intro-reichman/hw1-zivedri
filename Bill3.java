1eca4f52bcddf05d093afffbe3e156f5f9fcf681
ziveedri <ziv.edri01@runi.ac.il>
2024-11-07 18:19:13 +0200

    done.

--- Bill3.java
+++ Bill3.java
@@ -1,8 +1,11 @@
 // Splits a restaurant bill evenly among three diners.
 public class Bill3 {
 	public static void main(String[] args) {
-		// To get you started, here is the first line in the program:
 	    String name1 = args[0];
-	    // Replace this comment with the rest of your code   
+		String name2 = args[1];
+		String name3 = args[2];
+		int value = Integer.parseInt(args[3]);
+		System.out.println("dear "+name1+", "+name2+", and "+name3+": pay "+ Math.ceil((double)value/3)+" Shekels each.");
+
 	}
 }
--- FVCalc.java
+++ FVCalc.java
@@ -1,6 +1,9 @@
 // Computes the future value of a saving investment.
 public class FVCalc {
 	public static void main(String[] args){
-		// Replace this comment with your code
+		int currentValue = Integer.parseInt(args[0]);
+		double rate = Double.parseDouble(args[1]);
+		int n = Integer.parseInt(args[2]);
+		System.out.println("After "+n+" years, $"+currentValue+" saved at "+rate+"% will yield $"+(int)(currentValue*Math.pow((1+(rate/100)),n)));
 	}
 }
